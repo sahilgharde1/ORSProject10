6c85627bcb2494b1bd26d6838ed513f5bcda0cb8
sahilgharde1 <ghardesahil198@gmail.com>
2024-02-04 18:00:16 +0530

    msg

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Mr.Sahil Gharde
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
